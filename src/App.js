d1362a223bc07f5a3e068d1ee5468adb2696f285
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-04 01:32:05 +0100

    instizilizing google signin using firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Home,Login } from './component'
+import { getAuth } from 'firebase/auth'
+import { app } from './configuration/firebase.configuration'
 
 const App = () => {
+  const firebaseAuth = getAuth(app)
+  const nagivate = useNavigate();
+  const [auth,setAuth] = useState(false || window.localStorage.getItem("auth")==="true")
+  useEffect(()=>{
+      firebaseAuth.onAuthStateChanged((userCred)=>{
+        if(userCred){
+          userCred.getIdToken().then((token)=>{
+            console.log(token)
+          })
+        }else{
+          setAuth(false);
+          window.localStorage.setItem("auth", "false");
+          nagivate("./login")
+        }
+      })
+  },[])
   return (
     <div className='w-screen h-screen  bg-primary flex justify-center items-center'>
        <div className='w-screen-half flex flex-column'>
           <Routes>          
-            <Route path='/Login' element={<Login/>} />
+            <Route path='/Login' element={<Login setAuth={setAuth}/>} />
             <Route  path='/*'  element={<Home/>} />
           </Routes>
        </div>
--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {FcGoogle} from "react-icons/fc"
 import {app} from '../configuration/firebase.configuration';
 import {getAuth, GoogleAuthProvider, signInWithPopup}from 'firebase/auth'
 import { useNavigate } from 'react-router-dom';
-const Login = () => {
+const Login = ({setAuth}) => {
   const firebaseAuth = getAuth(app)
   const provider =new GoogleAuthProvider();
   const navigate  = useNavigate()
     const loginWithGoogle = async ()=>{
       await signInWithPopup(firebaseAuth, provider)
       .then((userCred)=>{
-          console.log(userCred)
+        if(userCred){
+          setAuth(true);
+          window.localStorage.setItem("auth", "true");
+          firebaseAuth.onAuthStateChanged((userCred)=>{
+            if(userCred){
+              userCred.getIdToken().then((token)=>{
+                console.log(token)
+              })
+              console.log(userCred);
+              navigate('/',{replace:true})
+            }else{
+              setAuth(false)
+              navigate("/login")
+            }
+          })
+        }
+          
       })
 
       console.log("iboytech got it");
     }
+    useEffect(()=>{
+        if(window.localStorage.getItem("auth")===true){
+          navigate('/',{replace:true})
+        }
+    },[])
   return (
     <div className='relative w-screen h-screen'>
         <div className='absolute inset-0 bg-darkOverlay flex justify-center p-4  items-center'>
