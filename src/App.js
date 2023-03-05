2fc75ac46ff1369cebb923480581995c1c627feb
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-05 21:12:25 +0100

    small changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,33 @@ import { Route, Routes, useNavigate } from 'react-router-dom'
 import { Home,Login } from './component'
 import { getAuth } from 'firebase/auth'
 import { app } from './configuration/firebase.configuration'
-
+// import { jwtDecode, jwtVerify, resignJwt } from 'jwt-js-decode';
 const App = () => {
   const firebaseAuth = getAuth(app)
   const nagivate = useNavigate();
-  const [auth,setAuth] = useState(false || window.localStorage.getItem("auth")==="true")
+  const [auth,setAuth] = useState(false || window.localStorage.getItem("auth")==="true");
+    //  // just decode 'token' into {header: Object, payload: Object, signature: String}
+    //  let jwt = jwtDecode('token');
+    //  console.log(jwt.payload);
+ 
+    //  // or verify 'token' with provided secret and decode it
+    //  jwtVerify('token', 'secret').then(res => {
+    //      if (res === true) {
+    //          const jwt = jwtDecode('token');
+    //          console.log(jwt.payload);
+    //      }
+    //  });
+     
+    //  // advanced resignJwt token with newSecret secret should be same type as jwt.header.alg
+    //  resignJwt('token', 'newSecret').then(newToken =>  {
+    //      console.log(newToken);
+    //  });
   useEffect(()=>{
       firebaseAuth.onAuthStateChanged((userCred)=>{
         if(userCred){
           userCred.getIdToken().then((token)=>{
-            console.log(token)
+            console.log("JWT token obtained successfully:", token);
+
           })
         }else{
           setAuth(false);
