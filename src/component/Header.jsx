ba7c45a438a88f9931c83b04430719bac45999e5
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-09 04:22:49 +0100

    getting user image and fix it to the nav bar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -6,7 +6,7 @@ import { isActiveStyles, isNotActiveStyles } from '../utils/styles';
 import {useStateValue} from '../Context/StateProvider'
 
 const Header = () => {
-  const [{user}, dispatch] = useStateValue();
+  const [{user}, dispatch] = useStateValue()
   return (    
        <header className="flex items-center w-full p-4 md:py-2 md:px-6">
             <NavLink to={"/"}>
@@ -19,9 +19,9 @@ const Header = () => {
                 <li className='mx-5  text-lg' ><NavLink className={({isActive})=>isActive ? isActiveStyles:isNotActiveStyles } to={"/contact us"}>Contact us</NavLink> </li>
             </ul>      
               <div className='flex  items-center ml-auto cursor-pointer gap-2 relative'>
-                    <img src="" className=' w-12 min-w[44px] object-cover round-full shadow-lg' alt="user-pic" />
+                    <img src={user?.user.imageURL} className=' w-12 h-12 min-w[44px] object-cover rounded-full shadow-lg' alt="user-pic" />
                   <div className='flex flex-col'>
-                  <p className='text-textColor text-lg hover:text-headingColor font-semibold'>{user?.user.name}</p>
+                  <p className='text-textColor text-lg hover:text-headingColor font-semibold'>{user?.user?.name}</p>
                     <p className='flex items-center gap-2 text-gray-500 font-normal'>Premium . <FaCrown className='text-sm -ml-1 text-yellow-500'/></p>
                   </div>
               </div>
