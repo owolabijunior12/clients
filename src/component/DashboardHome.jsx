994f33ee58c838372a4d95b1c83531420141cea9
Owolabi Destiny <107050551+owolabijunior12@users.noreply.github.com>
2023-03-23 14:20:02 +0100

    debugging

--- src/component/DashboardHome.jsx
+++ src/component/DashboardHome.jsx
@@ -1,17 +1,16 @@
 import React, { useEffect } from 'react';
-import { FaUsers } from "react-icons/fa";
-import { GiLoveSong, GiMusicalNotes } from "react-icons/gi/index.js";
-import { RiUserStarFill } from "react-icons/ri";
+import { FaUsers } from 'react-icons/fa';
+import { GiLoveSong, GiMusicalNotes } from 'react-icons/gi';
+import { RiUserStarFill } from 'react-icons/ri';
 import { useStateValue } from '../Context/StateProvider'; 
 import { getAllAlbums, getAllArtists, getAllSongs, getAllUser } from '../api/index';
 import { actionType } from '../Context/reducer';
-import {bgColors} from '../utils/styles'
-
+// import { bgColors } from '../utils/styles';
 
 export const DashboardCards = ({ icon, name, count }) => {
-  const bg_color =bgColors[parseInt(Math.random()*bgColors.length)]
+  // const bg_color = bgColors[parseInt(Math.random() * bgColors.length)];
   return (
-    <div style={{background:`${bg_color}`}} className="p-4 gap-3 h-auto rounded-lg shadow-md bg-textColor text-white">
+    <div className="p-4 gap-3 h-auto rounded-lg shadow-md bg-textColor text-white">
       {icon}
       <p className='text-xl text-white font-bold'>{name}</p>
       <p className='text-xl text-white'>{count}</p>
@@ -20,56 +19,55 @@ export const DashboardCards = ({ icon, name, count }) => {
 };
 
 const DashboardHome = () => {
-  const [{ allUsers, allSong, allAlbum, allArtist }, dispatch] = useStateValue();
-  
+  const [{ allUsers, allSongs, allAlbums, allArtists }, dispatch] = useStateValue();
+
   useEffect(() => {
-    if (allUsers) {
+    if (!allUsers) {
       getAllUser().then((data) => {
         dispatch({
           type: actionType.SET_ALL_USERS,
-          allUser: data
+          allUsers: data.users
         });
         console.log(data);
       });
     }
 
-    if (allSong) {
+    if (!allSongs) {
       getAllSongs().then((data) => {
         dispatch({
           type: actionType.SET_ALL_SONGS,
-          allSongs: data
+          allSongs: data.songs
         });
         console.log(data);
       });
     }
 
-    if (!allArtist) {
-      getAllArtists().then((data) => {
-        dispatch({
-          type: actionType.SET_ARTISTS,
-          artists: data
-        });
-        console.log(data);
-      });
-    }
-
-    if (!allAlbum) {
+    if (!allAlbums) {
       getAllAlbums().then((data) => {
         dispatch({
           type: actionType.SET_ALL_ALBUMS,
-          allAlbums: data
+          allAlbums: data.albums
         });
-        console.log(allAlbum);
+        console.log(data.albums);
+      });
+    }
+    if (!allArtists) {
+      getAllArtists().then((data) => {
+        dispatch({
+          type: actionType.SET_ARTISTS,
+          artists: data.artists
+        });
+        console.log(data.artists);
       });
     }
   }, []);
-      
-    return (
-    <div className='w-full snap-mandatory p-6 flex items-center text-black justify-evenly flex-wrap'>      
-      <DashboardCards icon={<FaUsers className="text-3xl " />} name={"Users"} count={allUsers?.length > 0 ? allUsers?.length : 0} />
-      <DashboardCards icon={<GiLoveSong className="text-3xl " />} name={"Songs"} count={allSong?.length > 0 ? allSong?.length : 0} />
-      <DashboardCards icon={<RiUserStarFill className="text-3xl " />} name={"Artist"} count={allArtist?.length > 0 ? allArtist?.length : 0} />
-      <DashboardCards icon={<GiMusicalNotes className="text-3xl " />} name={"Album"} count={allAlbum?.length > 0 ? allAlbum?.length : "hell"} />                    
+
+  return (
+    <div className="w-full snap-mandatory p-6 flex items-center text-black justify-evenly flex-wrap">      
+      <DashboardCards icon={<FaUsers className="text-3xl" />} name="Users" count={allUsers?.length ?? 0} />
+      <DashboardCards icon={<GiLoveSong className="text-3xl" />} name="Songs" count={allSongs?.length ?? 0} />
+      <DashboardCards icon={<RiUserStarFill className="text-3xl" />} name="Artists" count={allArtists?.length ?? 0} />
+      <DashboardCards icon={<GiMusicalNotes className="text-3xl" />} name="Albums" count={allAlbums?.length ?? 0} />                    
     </div>
   );
 };
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -15,55 +15,55 @@ export const actionType = {
 };
 
 const reducer = (state, action) => {
-  console.log(action);
+  
 
   switch (action.type) {
     case actionType.SET_USER:
       return {
         ...state,
-        user: action.user,
+        user: action.users,
       
       };
-    // case actionType.SET_SEARCH_TERM:
-    //   return {
-    //     ...state,
-    //     searchTerm: action.searchTerm,
-    //   };
+    case actionType.SET_SEARCH_TERM:
+      return {
+        ...state,
+        searchTerm: action.searchTerm,
+      };
 
-    // case actionType.SET_FILTER_TERM:
-    //   return {
-    //     ...state,
-    //     filterTerm: action.filterTerm,
-    //   };
+    case actionType.SET_FILTER_TERM:
+      return {
+        ...state,
+        filterTerm: action.filterTerm,
+      };
 
     case actionType.SET_ARTISTS:
       return {
         ...state,
-        artists: action.artists,
+        allArtists: action.artists,
       };
 
-    // case actionType.SET_ARTIST_FILTER:
-    //   return {
-    //     ...state,
-    //     artistFilter: action.artistFilter,
-    //   };
+    case actionType.SET_ARTIST_FILTER:
+      return {
+        ...state,
+        artistFilter: action.artistFilter,
+      };
 
-    // case actionType.SET_LANGUAGE_FILTER:
-    //   return {
-    //     ...state,
-    //     languageFilter: action.languageFilter,
-    //   };
+    case actionType.SET_LANGUAGE_FILTER:
+      return {
+        ...state,
+        languageFilter: action.languageFilter,
+      };
 
     case actionType.SET_ALL_USERS:
       return {
         ...state,
-        allUser: action.allUser,
+        allUser: action.user,
       };
 
     case actionType.SET_ALL_SONGS:
       return {
         ...state,
-        allSong: action.allSong,
+        allSong: action.songs,
       };
 
     case actionType.SET_ALL_ALBUMS:
@@ -72,29 +72,29 @@ const reducer = (state, action) => {
         allAlbums: action.allAlbums,
       };
 
-    // case actionType.SET_ALBUM_FILTER:
-    //   return {
-    //     ...state,
-    //     albumFilter: action.albumFilter,
-    //   };
+    case actionType.SET_ALBUM_FILTER:
+      return {
+        ...state,
+        albumFilter: action.albumFilter,
+      };
 
     case actionType.SET_SONG:
       return {
         ...state,
-        song: action.song,
+        allSong: action.song,
       };
 
-    // case actionType.SET_SONG_PLAYING:
-    //   return {
-    //     ...state,
-    //     isSongPlaying: action.isSongPlaying,
-    //   };
+    case actionType.SET_SONG_PLAYING:
+      return {
+        ...state,
+        isSongPlaying: action.isSongPlaying,
+      };
 
-    // case actionType.SET_MINI_PLAYER:
-    //   return {
-    //     ...state,
-    //     miniPlayer: action.miniPlayer,
-    //   };
+    case actionType.SET_MINI_PLAYER:
+      return {
+        ...state,
+        miniPlayer: action.miniPlayer,
+      };
 
     default:
       return state;
